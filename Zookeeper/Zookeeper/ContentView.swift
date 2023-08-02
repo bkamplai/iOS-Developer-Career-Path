1cdc690058e95746694a14113bedf2eff6a97d66
Brandon Kamplain <103218065+bkamplai@users.noreply.github.com>
2023-08-02 16:31:59 -0600

    Building Lists in SwiftUI

---
+++ Zookeeper/Zookeeper/ContentView.swift
@@ -0,0 +1,59 @@
+//
+//  ContentView.swift
+//  Zookeeper
+//
+//  Created by Brandon Kamplain on 8/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    let animalGroups = [
+        AnimalGroup(groupName: "Pets", animals: [
+            Animal(name: "Dog"),
+            Animal(name: "Cat"),
+            Animal(name: "Parrot"),
+            Animal(name: "Mouse")
+        ]),
+        AnimalGroup(groupName: "Farm", animals: [
+            Animal(name: "Cow"),
+            Animal(name: "Horse"),
+            Animal(name: "Goat"),
+            Animal(name: "Sheep"),
+        ]),
+        AnimalGroup(groupName: "Critters", animals: [
+            Animal(name: "Firefly"),
+            Animal(name: "Spider"),
+            Animal(name: "Ant"),
+            Animal(name: "Squirrel"),
+        ])
+    ]
+    
+    var body: some View {
+        List {
+            ForEach(animalGroups) { animalGroup in
+                Section(header: Text(animalGroup.groupName)) {
+                    ForEach(animalGroup.animals) { animal in
+                        Text(animal.name)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct Animal: Identifiable {
+    var id = UUID()
+    var name: String
+}
+
+struct AnimalGroup: Identifiable {
+    var id = UUID()
+    var groupName: String
+    var animals: [Animal]
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Zookeeper/Zookeeper/ZookeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZookeeperApp.swift
+//  Zookeeper
+//
+//  Created by Brandon Kamplain on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct ZookeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
