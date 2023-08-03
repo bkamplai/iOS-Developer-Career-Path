74b87c243c31a3753cea0ede3de463a71487e515
Brandon Kamplain <103218065+bkamplai@users.noreply.github.com>
2023-08-02 20:53:57 -0600

    Grids in SwiftUI

---
+++ GridCalendar/GridCalendar/GridCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridCalendarApp.swift
+//  GridCalendar
+//
+//  Created by Brandon Kamplain on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct GridCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
