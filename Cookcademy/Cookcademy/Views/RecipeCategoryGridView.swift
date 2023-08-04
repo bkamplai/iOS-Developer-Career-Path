01061a7d619b99a70123b4db780ab17fa6f06f51
Brandon Kamplain <103218065+bkamplai@users.noreply.github.com>
2023-08-03 20:01:42 -0600

    Cookcademy 3 - Build the Recipe Category Grid View

---
+++ Cookcademy/Cookcademy/Views/RecipeCategoryGridView.swift
@@ -0,0 +1,50 @@
+//
+//  RecipeCategoryGridView.swift
+//  Cookcademy
+//
+//  Created by Brandon Kamplain on 8/3/23.
+//
+
+import SwiftUI
+
+struct RecipeCategoryGridView: View {
+    @StateObject private var recipeData = RecipeData()
+    
+    var body: some View {
+        let columns = [GridItem(), GridItem()]
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns, content: {
+                    ForEach(MainInformation.Category.allCases, id: \.self) { category in
+                        NavigationLink(
+                            destination: RecipesListView(category: category)
+                                .environmentObject(recipeData),
+                            label: {
+                                CategoryView(category: category)
+                            })
+                    }
+                })
+            }
+            .navigationTitle("Categories")
+        }
+    }
+}
+
+struct CategoryView: View {
+    let category: MainInformation.Category
+    
+    var body: some View {
+        ZStack {
+            Image(category.rawValue)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .opacity(0.35)
+            Text(category.rawValue)
+                .font(.title)
+        }
+    }
+}
+
+#Preview {
+    RecipeCategoryGridView()
+}
