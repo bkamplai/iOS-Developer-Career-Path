92fd7577e49176b66c92623c622d324f6d9d9a74
Brandon Kamplain <103218065+bkamplai@users.noreply.github.com>
2023-08-05 14:54:20 -0600

    Cookcademy 6 - Build a Form to Edit Ingredients

---
+++ Cookcademy/Cookcademy/Views/ModifyRecipes/ModifyIngredientView.swift
@@ -0,0 +1,73 @@
+//
+//  ModifyIngredientView.swift
+//  Cookcademy
+//
+//  Created by Brandon Kamplain on 8/5/23.
+//
+
+import SwiftUI
+
+struct ModifyIngredientView: View {
+    @Binding var ingredient: Ingredient
+    let createAction: ((Ingredient) -> Void)
+    
+    @Environment(\.presentationMode) private var mode
+    
+    private let listBackgroundColor = AppColor.background
+    private let listTextColor = AppColor.foreground
+    
+    var body: some View {
+        VStack {
+            Form {
+                TextField("Ingredient Name", text: $ingredient.name)
+                    .listRowBackground(listBackgroundColor)
+                Stepper(value: $ingredient.quantity, in: 0...100, step: 0.5) {
+                    HStack {
+                        Text("Quantity:")
+                        TextField("Quantity", value: $ingredient.quantity, formatter: NumberFormatter.decimal)
+                            .keyboardType(.numbersAndPunctuation)
+                    }
+                }.listRowBackground(listBackgroundColor)
+                Picker(selection: $ingredient.unit, label: HStack {
+                    Text("Unit")
+                    Spacer()
+//                    Text(ingredient.unit.rawValue)
+                }) {
+                    ForEach(Ingredient.Unit.allCases, id: \.self) { unit in
+                        Text(unit.rawValue)
+                    }
+                }.listRowBackground(listBackgroundColor)
+                .pickerStyle(MenuPickerStyle())
+                HStack {
+                    Spacer()
+                    Button("Save") {
+                        createAction(ingredient)
+                        mode.wrappedValue.dismiss()
+                    }
+                    Spacer() 
+                }.listRowBackground(listBackgroundColor)
+            }
+            .foregroundColor(listTextColor)
+        }
+    }
+}
+
+extension NumberFormatter {
+    static var decimal: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        return formatter
+    }
+}
+
+struct ModifyIngredientView_Previews: PreviewProvider {
+    @State static var emptyIngredient = Ingredient()
+    
+    static var previews: some View {
+        NavigationView {
+            ModifyIngredientView(ingredient: $emptyIngredient) { ingredient in
+                print(ingredient)
+            }
+        }
+    }
+}
---
+++ Cookcademy/Cookcademy/Views/ModifyRecipes/ModifyIngredientsView.swift
@@ -0,0 +1,50 @@
+//
+//  ModifyIngredientsView.swift
+//  Cookcademy
+//
+//  Created by Brandon Kamplain on 8/5/23.
+//
+
+import SwiftUI
+
+struct ModifyIngredientsView: View {
+    @Binding var ingredients: [Ingredient]
+    @State private var newIngredient = Ingredient()
+    
+    private let listBackgroundColor = AppColor.background
+    private let listTextColor = AppColor.foreground
+    
+    var body: some View {
+        VStack {
+            let addIngredientView = ModifyIngredientView(ingredient: $newIngredient) { ingredient in
+                ingredients.append(ingredient)
+                newIngredient = Ingredient()
+            }
+            if ingredients.isEmpty {
+                Spacer()
+                NavigationLink("Add the first ingredient", destination: addIngredientView)
+                Spacer()
+            } else {
+                List {
+                    ForEach(ingredients.indices, id: \.self) { index in
+                        let ingredient = ingredients[index]
+                        Text(ingredient.description)
+                    }
+                    .listRowBackground(listBackgroundColor)
+                    NavigationLink("Add another ingredient", destination: addIngredientView)
+                        .buttonStyle(PlainButtonStyle())
+                        .listRowBackground(listBackgroundColor)
+                }.foregroundColor(listTextColor)
+            }
+        }
+    }
+}
+
+struct ModifyIngredientsView_Previews: PreviewProvider {
+    @State static var emptyIngredients = [Ingredient]()
+    static var previews: some View {
+        NavigationView {
+            ModifyIngredientsView(ingredients: $emptyIngredients)
+        }
+    }
+}
