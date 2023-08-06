23a1798b1ca42908980b3facd81b2d87244da6b1
Brandon Kamplain <103218065+bkamplai@users.noreply.github.com>
2023-08-06 15:06:20 -0600

    Cookcademy 10 - Persist Settings with AppStorage

--- Cookcademy/Cookcademy/Views/ExploreRecipes/RecipeDetailView.swift
+++ Cookcademy/Cookcademy/Views/ExploreRecipes/RecipeDetailView.swift
@@ -10,8 +10,9 @@ import SwiftUI
 struct RecipeDetailView: View {
     @Binding var recipe: Recipe
     
-    private let listBackgroundColor = AppColor.background
-    private let listTextColor = AppColor.foreground
+    @AppStorage("listBackgroundColor") private var listBackgroundColor = AppColor.background
+    @AppStorage("listTextColor") private var listTextColor = AppColor.foreground
+    @AppStorage("hideOptionalSteps") private var hideOptionalSteps: Bool = false
     
     @State private var isPresenting = false
     
@@ -40,10 +41,15 @@ struct RecipeDetailView: View {
                 Section(header: Text("Directions")) {
                     ForEach(recipe.directions.indices, id: \.self) { index in
                         let direction = recipe.directions[index]
-                        HStack {
-                            Text("\(index + 1). ").bold()
-                            Text("\(direction.isOptional ? "(Optional) " : "")" + "\(direction.description)")
-                        }.foregroundColor(listTextColor)
+                        if direction.isOptional && hideOptionalSteps {
+                            EmptyView()
+                        } else {
+                            HStack {
+                                let index = recipe.index(of: direction, excludingOptionalDirections: hideOptionalSteps) ?? 0
+                                Text("\(index + 1). ").bold()
+                                Text("\(direction.isOptional ? "(Optional) " : "")" + "\(direction.description)")
+                            }.foregroundColor(listTextColor)
+                        }
                     }
                 }.listRowBackground(listBackgroundColor)
             }
---
+++ Cookcademy/Cookcademy/Views/SettingsView.swift
@@ -0,0 +1,36 @@
+//
+//  SettingsView.swift
+//  Cookcademy
+//
+//  Created by Brandon Kamplain on 8/6/23.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    @AppStorage("hideOptionalSteps") private var hideOptionalSteps: Bool = false
+    @AppStorage("listBackgroundColor") private var listBackgroundColor = AppColor.background
+    @AppStorage("listTextColor") private var listTextColor = AppColor.foreground
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                ColorPicker("List Background Color", selection: $listBackgroundColor)
+                    .padding()
+                    .listRowBackground(listBackgroundColor)
+                ColorPicker("Text Color", selection: $listTextColor)
+                    .padding()
+                    .listRowBackground(listBackgroundColor)
+                Toggle("Hide Optional Steps", isOn: $hideOptionalSteps)
+                    .padding()
+                    .listRowBackground(listBackgroundColor)
+            }
+            .foregroundColor(listTextColor)
+            .navigationTitle("Settings")
+        }
+    }
+}
+
+#Preview {
+    SettingsView()
+}
