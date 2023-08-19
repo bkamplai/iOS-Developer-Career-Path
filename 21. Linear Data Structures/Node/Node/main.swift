763e2a5cd1448ac36f847c14da95092dec5bf3a2
Brandon Kamplain <103218065+bkamplai@users.noreply.github.com>
2023-08-19 11:04:36 -0600

    Defining the Next Node

--- 21. Linear Data Structures/Node/Node/main.swift
+++ 21. Linear Data Structures/Node/Node/main.swift
@@ -7,6 +7,7 @@
 
 class Node {
     var data: String
+    var next: Node?
     
     init(data: String) {
         self.data = data
@@ -14,5 +15,14 @@ class Node {
 }
 
 var firstNode = Node(data: "I am a Node!")
-
 print(firstNode.data)
+
+var nodeOne = Node(data: "Node 1")
+if let node = nodeOne.next {
+    print("The next node of nodeOne is \(node)")
+} else {
+    print("The next node of nodeOne is nil")
+}
+var nodeTwo = Node(data: "Node 2")
+nodeOne.next = nodeTwo
+print(nodeOne.next!.data)
