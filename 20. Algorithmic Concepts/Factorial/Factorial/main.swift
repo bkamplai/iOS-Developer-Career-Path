8fc94867cb06321dd0392818a2e0f3dee09f6473
Brandon Kamplain <103218065+bkamplai@users.noreply.github.com>
2023-08-18 23:46:55 -0600

    The Base Case

--- 20. Algorithmic Concepts/Factorial/Factorial/main.swift
+++ 20. Algorithmic Concepts/Factorial/Factorial/main.swift
@@ -20,8 +20,9 @@ func findFactorialRecursively(of n: Int) -> Int {
     if n > 0 {
         print("Execution context: " + String(n))
         return n * findFactorialRecursively(of: n - 1)
+    } else {
+        return 1
     }
-    return 0
 }
 
 var fourFactorial = findFactorialIteratively(of:4)
