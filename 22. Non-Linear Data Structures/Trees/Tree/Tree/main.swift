8a9af0329e856a764798eba7ab6a04bc799fa051
Brandon Kamplain <103218065+bkamplai@users.noreply.github.com>
2023-12-15 16:32:18 -0700

    Growing The Branches and Leaves

--- 22. Non-Linear Data Structures/Trees/Tree/Tree/main.swift
+++ 22. Non-Linear Data Structures/Trees/Tree/Tree/main.swift
@@ -13,7 +13,20 @@ class TreeNode {
         self.data = data
         self.children = []
     }
+    
+    func addChild(_ newChild: TreeNode) {
+        children.append(newChild)
+    }
+    
+    func addChild(_ newChildData: String) {
+        children.append(TreeNode(data: newChildData))
+    }
 }
 
 var root = TreeNode(data: "Planting my first seed!")
-print(root.data)    //Prints: Planting my first seed!
+print(root.data)                //Prints: Planting my first seed!
+print(root.children.isEmpty)    //Prints: true
+var branch1 = TreeNode(data: "Your tree is ")
+root.addChild(branch1)
+root.addChild("growing every day.")
+print(root.children.isEmpty)    //Prints: false
