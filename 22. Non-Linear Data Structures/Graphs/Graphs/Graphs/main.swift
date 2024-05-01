f961e391899559579a60d61b7376698ff0903ae5
Brandon Kamplain <103218065+bkamplai@users.noreply.github.com>
2024-04-30 19:14:29 -0600

    Implementing Graph Edges

--- 22. Non-Linear Data Structures/Graphs/Graphs/Graphs/main.swift
+++ 22. Non-Linear Data Structures/Graphs/Graphs/Graphs/main.swift
@@ -5,6 +5,16 @@
 //  Created by Brandon Kamplain on 4/30/24.
 //
 
+struct GraphEdge {
+    let nodeOne: GraphNode
+    let nodeTwo: GraphNode
+    
+    init(nodeOne: GraphNode, nodeTwo: GraphNode) {
+        self.nodeOne = nodeOne
+        self.nodeTwo = nodeTwo
+    }
+}
+
 class GraphNode {
     var data: String
     var neighboringNodes: [GraphNode]
@@ -39,14 +49,27 @@ extension GraphNode: CustomStringConvertible {
 
 class Graph {
     var nodes: [GraphNode]
+    var edges: [GraphEdge]
     
     init(nodes: [GraphNode]) {
         self.nodes = nodes
+        edges = []
     }
     
     func printGraph() {
         for node in nodes {
             Swift.print("\(node): \(node.neighboringNodes)")
+        }
+    }
+    
+    func addEdge(from nodeOne: GraphNode, to nodeTwo: GraphNode) {
+        edges.append(GraphEdge(nodeOne: nodeOne, nodeTwo: nodeTwo))
+        nodeOne.addNeighbor(nodeTwo)
+    }
+    
+    func addEdges(from nodeOne: GraphNode, to neighboringNodes: [GraphNode]) {
+        for node in neighboringNodes {
+            addEdge(from: nodeOne, to: node)
         }
     }
 }
