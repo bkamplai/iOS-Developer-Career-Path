6292b2be2d16a97e4df25f8f1b6fe843e37a22bb
Brandon Kamplain <103218065+bkamplai@users.noreply.github.com>
2024-05-27 15:51:58 -0600

    Finding the Maximum Value

--- 23. Search Algorithms/Linear Search/LinearSearch/LinearSearch/main.swift
+++ 23. Search Algorithms/Linear Search/LinearSearch/LinearSearch/main.swift
@@ -36,18 +36,18 @@ func maximumValue<T: Comparable>(in arr: [T]) -> T? {
 let values = [ 10, 14, 19, 26, 27, 31, 33, 35, 42, 44]
 let targetValue = 100//33
 
-let tourLocations = ["New York City", "Los Angeles", "Bangkok", "Istanbul", "London", "New York City", "Toronto"]
-let targetCity = "New York City"
-
-let testScores = [88, 93, 75, 100, 80, 67, 71, 92, 90, 83]
-
 if let index = firstIndex(of: targetValue, in: values) {
     print("Value found at index \(index)")
 } else {
     print("Value not found")
 }
 
+let tourLocations = ["New York City", "Los Angeles", "Bangkok", "Istanbul", "London", "New York City", "Toronto"]
+let targetCity = "New York City"
+
 print(allIndices(matching: targetCity, in: tourLocations))
+
+let testScores = [88, 93, 75, 100, 80, 67, 71, 92, 90, 83]
 
 if let topScore = maximumValue(in: testScores) {
     print("The top score is \(topScore)")
