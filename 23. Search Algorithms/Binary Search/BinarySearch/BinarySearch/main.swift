52c4245743c320c27517efdddc4ccf57d7e3dbc0
Brandon Kamplain <103218065+bkamplai@users.noreply.github.com>
2024-05-27 16:08:35 -0600

    Introduction and Empty Base Case

---
+++ 23. Search Algorithms/Binary Search/BinarySearch/BinarySearch/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  BinarySearch
+//
+//  Created by Brandon Kamplain on 5/27/24.
+//
+
+func binarySearch<T: Comparable>(for key: T, in arr: [T], withRange range: Range<Int>) -> Int? {
+    guard range.lowerBound < range.upperBound else {
+        return nil
+    }
+    return nil
+}
