69aab07125cc35c4a49e0778379f821e10ce30d2
Brandon Kamplain <103218065+bkamplai@users.noreply.github.com>
2024-05-27 16:44:16 -0600

    Review

--- 23. Search Algorithms/Binary Search/BinarySearch/BinarySearch/main.swift
+++ 23. Search Algorithms/Binary Search/BinarySearch/BinarySearch/main.swift
@@ -6,13 +6,17 @@
 //
 
 func binarySearch<T: Comparable>(for key: T, in arr: [T], withRange range: Range<Int>) -> Int? {
+    print("Searching for \(key) in \(arr[range.lowerBound..<range.upperBound])")
+    
     guard range.lowerBound < range.upperBound else {
+        print("Invalid range, returning nil")
         return nil
     }
     
     let midIndex = (range.lowerBound + (range.upperBound - range.lowerBound)) / 2
     
     if arr[midIndex] == key {
+        print("Found \(key) at index \(midIndex)")
         return midIndex
     }
     
@@ -24,4 +28,10 @@ func binarySearch<T: Comparable>(for key: T, in arr: [T], withRange range: Range
         newRange = range.lowerBound..<midIndex
     }
     return binarySearch(for: key, in: arr, withRange: newRange)
+}
+
+let data = [1, 14, 23, 33, 56, 100, 110, 125, 140]
+let target = 23
+if let index = binarySearch(for: target, in: data, withRange: 0..<data.count) {
+    print(index)
 }
