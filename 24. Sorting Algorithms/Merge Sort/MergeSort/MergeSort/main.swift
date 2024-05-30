d0837d2e9bb948a97e5a6a1409a45f896b568dd3
Brandon Kamplain <103218065+bkamplai@users.noreply.github.com>
2024-05-29 20:02:52 -0600

    Splitting: Recursive Case

--- 24. Sorting Algorithms/Merge Sort/MergeSort/MergeSort/main.swift
+++ 24. Sorting Algorithms/Merge Sort/MergeSort/MergeSort/main.swift
@@ -7,5 +7,10 @@
 
 func mergeSort(_ inputArray: [Int]) -> [Int] {
     guard inputArray.count > 1 else {return inputArray}
+    let midIndex = inputArray.count / 2
+    let leftArray = Array(inputArray[0..<midIndex])
+    let rightArray = Array(inputArray[midIndex..<inputArray.count])
+    let leftMerge = mergeSort(leftArray)
+    let rightMerge = mergeSort(rightArray)
     return []
 }
