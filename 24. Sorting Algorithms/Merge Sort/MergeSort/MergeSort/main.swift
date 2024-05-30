d55869b4a453f69bd05d550fd6eedbcf0c5c22fb
Brandon Kamplain <103218065+bkamplai@users.noreply.github.com>
2024-05-29 19:37:35 -0600

    Splitting: Base Case

---
+++ 24. Sorting Algorithms/Merge Sort/MergeSort/MergeSort/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  MergeSort
+//
+//  Created by Brandon Kamplain on 5/29/24.
+//
+
+func mergeSort(_ inputArray: [Int]) -> [Int] {
+    guard inputArray.count > 1 else {return inputArray}
+    return []
+}
