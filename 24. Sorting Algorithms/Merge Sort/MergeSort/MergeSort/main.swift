e942ef31bd607c2263f1c497a5a04c9c34624075
Brandon Kamplain <103218065+bkamplai@users.noreply.github.com>
2024-05-29 20:21:34 -0600

    Merging

--- 24. Sorting Algorithms/Merge Sort/MergeSort/MergeSort/main.swift
+++ 24. Sorting Algorithms/Merge Sort/MergeSort/MergeSort/main.swift
@@ -16,5 +16,21 @@ func mergeSort(_ inputArray: [Int]) -> [Int] {
 }
 
 func merge(leftArray: [Int], rightArray: [Int]) -> [Int] {
-    return []
+    var leftIndex = 0
+    var rightIndex = 0
+    var orderedArray: [Int] = []
+    
+    while leftIndex < leftArray.count && rightIndex < rightArray.count {
+        let leftElement = leftArray[leftIndex]
+        let rightElement = rightArray[rightIndex]
+        
+        if leftElement < rightElement {
+            orderedArray.append(leftElement)
+            leftIndex += 1
+        } else {
+            orderedArray.append(rightElement)
+            rightIndex += 1
+        }
+    }
+    return orderedArray + Array(leftArray.dropFirst(leftIndex)) + Array(rightArray.dropFirst(rightIndex))
 }
